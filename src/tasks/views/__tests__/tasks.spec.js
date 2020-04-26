4dc12bdac6983cbdda916ba86f40e3a4b0ddb3cf
akeem <akeem.philbert@wepala.com>
2020-04-26 17:29:18 -0400

    tried adding ui tests but ran into issue with jest not able to transform flow file. I did try to configure flow as well as the strip flow plugin but no luck

---
+++ src/tasks/views/__tests__/tasks.spec.js
@@ -0,0 +1,10 @@
+import {render, fireEvent} from 'react-native-testing-library';
+// import {Tasks} from '../screens/Tasks';
+import React from 'react';
+
+describe.skip('tasks navigator', () => {
+  it('render navigation to view today and backlog', async () => {
+    // const {getAllByTestId} = render(<Tasks />);
+    // expect(getAllByTestId('TasksNavigator')).toHaveLength(1);
+  });
+});
