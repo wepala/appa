7cf8cb8ff38d0fa3299536c7ad6e3d59d7ee23f1
Simon Sankar <simon.sankar@wepala.com>
2020-05-03 19:15:18 -0400

    AGENDA-73 Fixed safe area view on agendas page

--- src/tasks/views/screens/Main.js
+++ src/tasks/views/screens/Main.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import List from './List';
 import TaskDetail from '../../controllers/Detail';
+import {SafeAreaView} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 const {Navigator, Screen} = createStackNavigator();
 
 export default () => (
-  <Navigator screenOptions={{gestureEnabled: false, headerShown: false}}>
-    <Screen name="TaskList" component={List} />
-    <Screen
-      name="CreateTask"
-      component={TaskDetail}
-      initialParams={{itemId: ''}}
-    />
-    <Screen
-      name="UpdateTask"
-      component={TaskDetail}
-      initialParams={{itemId: ''}}
-    />
-  </Navigator>
+  <SafeAreaView style={{flex: 1}}>
+    <Navigator screenOptions={{gestureEnabled: false, headerShown: false}}>
+      <Screen name="TaskList" component={List} />
+      <Screen
+        name="CreateTask"
+        component={TaskDetail}
+        initialParams={{itemId: ''}}
+      />
+      <Screen
+        name="UpdateTask"
+        component={TaskDetail}
+        initialParams={{itemId: ''}}
+      />
+    </Navigator>
+  </SafeAreaView>
 );
