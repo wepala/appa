fa08c83935cabd7ab6fa4f517ff2c41c34cb3ef5
akeem <akeem.philbert@wepala.com>
2020-05-03 18:42:12 -0400

    AGENDA-24 create a view selector to show the list of tasks in descending order
    * Updated the backlog controller to use new controller class
    * Created new getIncompleteTasks reselector
    * Connected the backlog controller to the backlog view

--- src/tasks/model/__tests__/selectors.spec.js
+++ src/tasks/model/__tests__/selectors.spec.js
@@ -1,5 +1,5 @@
 import {mockTasks} from '../../__tests__/fixtures';
-import {getTasksByDate} from '../selectors';
+import {getTasksByDate, getIncompleteTasks} from '../selectors';
 
 describe('Task Selectors', () => {
   it('should have a selector for getting tasks that are scheduled for a specified day', () => {
@@ -8,6 +8,14 @@ describe('Task Selectors', () => {
       tasks: mockTasks,
     };
     const items = getTasksByDate(mockState.tasks, today.toISOString());
+    expect(items).toBeArrayOfSize(4);
+  });
+
+  it('should have a selector for getting tasks that are not completed', () => {
+    const mockState = {
+      tasks: mockTasks,
+    };
+    const items = getIncompleteTasks(mockState.tasks);
     expect(items).toBeArrayOfSize(4);
   });
 });
--- src/tasks/views/components/Backlog.js
+++ src/tasks/views/components/Backlog.js
@@ -3,10 +3,10 @@ import React from 'react';
 import {StyleSheet} from 'react-native';
 import BacklogItem from './BacklogItem';
 
-export default ({navigation, data, contentContainerStyle}) => {
+export default ({navigation, items, contentContainerStyle}) => {
   const onItemPress = index => {
     navigation.navigate('UpdateTask', {
-      itemId: data[index].id,
+      itemId: items[index].id,
     });
   };
 
@@ -19,7 +19,7 @@ export default ({navigation, data, contentContainerStyle}) => {
     <List
       contentContainerStyle={[styles.container, contentContainerStyle]}
       numColumns={3}
-      data={data}
+      data={items}
       renderItem={renderItem}
     />
   );
