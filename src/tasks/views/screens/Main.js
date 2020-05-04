cc60352a887a204a1a354637f95df080173c1a6f
Simon Sankar <simon.sankar@wepala.com>
2020-05-04 11:08:19 -0400

    AGENDA-74 Fixed title reads from props

--- src/tasks/views/screens/Main.js
+++ src/tasks/views/screens/Main.js
@@ -2,21 +2,24 @@ import React from 'react';
 import List from './List';
 import TaskDetail from '../../controllers/Detail';
 import {createStackNavigator} from '@react-navigation/stack';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 const {Navigator, Screen} = createStackNavigator();
 
 export default () => (
-  <Navigator screenOptions={{gestureEnabled: false, headerShown: false}}>
-    <Screen name="TaskList" component={List} />
-    <Screen
-      name="CreateTask"
-      component={TaskDetail}
-      initialParams={{itemId: ''}}
-    />
-    <Screen
-      name="UpdateTask"
-      component={TaskDetail}
-      initialParams={{itemId: ''}}
-    />
-  </Navigator>
+  <SafeAreaView style={{flex: 1}}>
+    <Navigator screenOptions={{gestureEnabled: false, headerShown: false}}>
+      <Screen name="TaskList" component={List} />
+      <Screen
+        name="CreateTask"
+        component={TaskDetail}
+        initialParams={{itemId: ''}}
+      />
+      <Screen
+        name="UpdateTask"
+        component={TaskDetail}
+        initialParams={{itemId: ''}}
+      />
+    </Navigator>
+  </SafeAreaView>
 );
