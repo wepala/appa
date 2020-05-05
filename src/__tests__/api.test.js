54a8f2ab9209a424b9765f9993b47a9e0c3b7c2d
chad <timothy.kyadondo@gmail.com>
2020-05-05 17:10:13 +0300

    AGENDA-99: Add api tests

---
+++ src/__tests__/api.test.js
@@ -0,0 +1,19 @@
+import * as api from '../api.js';
+import events from '../__mocks__/events';
+
+import axios from 'axios';
+
+jest.mock('axios');
+
+describe('API', () => {
+  it('Should fetch events', async () => {
+    axios.get.mockResolvedValue({data: events});
+    let fetchEvents = await api.fetchEvents();
+    expect(fetchEvents).toEqual(events);
+  });
+
+  it('Should throw errors on request failure', async () => {
+    axios.get.mockRejectedValue(new Error('Fetch Failed'));
+    await expect(api.fetchEvents()).rejects.toThrow('Fetch Failed');
+  });
+});
