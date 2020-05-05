af98aed7a90bf9887878eea8b5392d04a4367126
Simon Sankar <simon.sankar@wepala.com>
2020-05-05 14:38:02 -0400

    AGENDA-73 Added basic test for current task component

--- src/tasks/views/components/CurrentTask.js
+++ src/tasks/views/components/CurrentTask.js
@@ -1,18 +1,26 @@
 import React from 'react';
 
-import {Text, Card, Layout, StyleService, Button} from '@ui-kitten/components';
+import {
+  Text,
+  Card,
+  Layout,
+  StyleService,
+  Button,
+  useStyleSheet,
+} from '@ui-kitten/components';
 
 export default ({item, index, onPress}) => {
+  const styles = useStyleSheet(themedStyles);
   return (
-    <Card style={styles.item} onPress={onPress}>
+    <Card testID="CurrentTask" style={styles.item} onPress={onPress}>
       <Layout style={styles.row}>
         <Layout style={styles.column1}>
           <Text category="s1">{item.title}</Text>
-          <Text>Project Name</Text>
+          {item.project && <Text category="s2">{item.project}</Text>}
         </Layout>
         <Layout style={styles.column2}>
-          <Text category="h5">4h 30m 52s</Text>
-          <Button style={styles.button} status="danger">
+          <Text category="h5">{item.totalTime}</Text>
+          <Button testID="TaskButton" style={styles.button} status="danger">
             Stop
           </Button>
         </Layout>
@@ -21,8 +29,9 @@ export default ({item, index, onPress}) => {
   );
 };
 
-const styles = StyleService.create({
+const themedStyles = StyleService.create({
   item: {
+    backgroundColor: 'background-basic-color-2',
     padding: 0,
     marginBottom: 16,
   },
---
+++ src/tasks/views/components/__tests__/Currentask.test.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {render, fireEvent} from 'react-native-testing-library';
+import {ApplicationProvider, IconRegistry, Button} from '@ui-kitten/components';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
+import * as eva from '@eva-design/eva';
+import {default as theme} from '../../../../../theme.json';
+import CurrentTask from '../CurrentTask';
+
+describe('Current Task component', () => {
+  const title = 'My Task';
+  const totalTime = '4h 23m 32s';
+  const project = 'Projecct X';
+
+  it('Should render a correctly given item prop', async () => {
+    const itemData = {
+      title,
+      totalTime,
+      project,
+    };
+    const onPressItem = jest.fn();
+    const {getAllByText, getAllByTestId} = render(
+      <>
+        <IconRegistry icons={EvaIconsPack} />
+        <ApplicationProvider
+          {...eva}
+          theme={{
+            ...eva.light,
+            ...theme,
+          }}>
+          <CurrentTask item={itemData} onPress={onPressItem} />
+        </ApplicationProvider>
+      </>,
+    );
+
+    // Task Item
+    const item = getAllByTestId('CurrentTask');
+    expect(item).toHaveLength(1);
+    fireEvent.press(item[0]);
+    expect(onPressItem).toHaveBeenCalled();
+
+    // Task text
+    const taskTitle = getAllByText(title);
+    const taskTotalTime = getAllByText(`${totalTime}`);
+    const taskProject = getAllByText(project);
+    expect(taskTitle).toHaveLength(1);
+    expect(taskTotalTime).toHaveLength(1);
+    expect(taskProject).toHaveLength(1);
+
+    // // Task Button
+    const button = getAllByTestId('TaskButton');
+    expect(button).toHaveLength(1);
+  });
+});
