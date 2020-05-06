3b23c1891444e844e3a7593a2704a7c62132dedb
chad <timothy.kyadondo@gmail.com>
2020-05-06 12:59:36 +0300

    AGENDA-99: Add reducers to rootReducer

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -2,11 +2,15 @@ import {combineReducers} from 'redux';
 import tasks from './tasks/model/reducer';
 import projects from './projects/model/reducer';
 import onboard from './onboarding/model/reducer';
+import {applicationId, token, eventLastCount} from './reducers';
 
 const rootReducer = combineReducers({
   tasks,
   onboard,
   projects,
+  applicationId,
+  token,
+  eventLastCount,
 });
 
 export default rootReducer;
