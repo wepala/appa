08dc6060919cc79031541e173a36de7410893ec2
chad <timothy.kyadondo@gmail.com>
2020-05-06 13:02:47 +0300

    AGENDA-99: Update store with state

--- src/store.js
+++ src/store.js
@@ -15,6 +15,9 @@ const initialState = {
       },
     },
   },
+  eventLastCount: 0,
+  token: null,
+  applicationId: '36212c03-040b-4139-867f-bd76485f4084', // TODO add acutal applicationId
 };
 const middlewares = [thunk];
 
