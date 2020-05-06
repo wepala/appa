1b45f07c62eaad36f303fabefca256f4cdabf2ad
chad <timothy.kyadondo@gmail.com>
2020-05-06 13:21:43 +0300

    AGENDA-99: Fix failing tests

--- src/apis/__tests__/api.spec.js
+++ src/apis/__tests__/api.spec.js
@@ -1,17 +1,16 @@
 import * as api from '../eventApi';
 import events from '../../__mocks__/events';
-
-import axios from 'axios';
+import {eventsApi} from '../calls';
 
 describe('API', () => {
   it('Should fetch events', async () => {
-    axios.get.mockResolvedValue({data: events});
+    eventsApi.get = jest.fn().mockResolvedValue({data: events});
     let fetchEvents = await api.fetchEvents();
     expect(fetchEvents).toEqual(events);
   });
 
   it('Should throw errors on request failure', async () => {
-    axios.get.mockRejectedValue(new Error('Fetch Failed'));
+    eventsApi.get = jest.fn().mockRejectedValue(new Error('Fetch Failed'));
     await expect(api.fetchEvents()).rejects.toThrow('Fetch Failed');
   });
 });
