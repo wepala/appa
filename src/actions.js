f28b4ce500c9584d65950be89c76dfae16d81dbe
chad <timothy.kyadondo@gmail.com>
2020-05-06 12:57:10 +0300

    AGENDA-99: Add actions

---
+++ src/actions.js
@@ -0,0 +1,26 @@
+import {
+  SET_EVENT_LAST_COUNT,
+  SET_TOKEN,
+  SET_APPLICATION_ID,
+} from './actionTypes';
+
+export const setEventLastCount = lastCount => {
+  return {
+    type: SET_EVENT_LAST_COUNT,
+    payload: lastCount,
+  };
+};
+
+export const setToken = token => {
+  return {
+    type: SET_TOKEN,
+    payload: token,
+  };
+};
+
+export const setApplicationId = applicationId => {
+  return {
+    type: SET_APPLICATION_ID,
+    payload: applicationId,
+  };
+};
