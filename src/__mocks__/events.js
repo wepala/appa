c0f3c9416599c0c00ba6c63694284f3e59156e6b
chad <timothy.kyadondo@gmail.com>
2020-05-07 12:34:28 +0300

    AGENDA-100: Add events

--- src/__mocks__/events.js
+++ src/__mocks__/events.js
@@ -44,6 +44,38 @@ const events = [
     meta: {
       id: '56268018-3057-4a75-80f2-281df7221c9f',
       seqeuenceNo: 2,
+      created: '2020-04-14',
+    },
+  },
+  {
+    type: 'REMOVE_TASK',
+    payload: {
+      id: '56268018-3057-4a75-80f2-281df7221c9b',
+      title: 'Complete React tutoral',
+      description: 'Finish effects, state, reducers',
+      project: 'wepala',
+      created: '2020-04-26',
+      dueDate: '2020-05-26',
+    },
+    meta: {
+      id: '56268018-3057-4a75-80f2-281df7221c9i',
+      seqeuenceNo: 1,
+      created: '2020-04-14T20:00:59+00:00',
+    },
+  },
+  {
+    type: 'UPDATE_TASK',
+    payload: {
+      id: '56268018-3057-4a75-80f2-281df7221c9C',
+      title: 'Complete lumen tutoral',
+      description: 'Finish effects, state, reducers',
+      project: 'wepala',
+      created: '2020-04-26',
+      dueDate: '2020-05-26',
+    },
+    meta: {
+      id: '56268018-3057-4a75-80f2-281df7221c9k',
+      seqeuenceNo: 2,
       created: '2020-04-14T20:00:59+00:00',
     },
   },
