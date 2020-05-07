f64d5be1bf1d9cf5361150589baa6284d1deaa87
chad <timothy.kyadondo@gmail.com>
2020-05-07 13:11:09 +0300

    AGENDA-100: Add mocks for tasks

---
+++ src/tasks/model/__mocks__/tasks.js
@@ -0,0 +1,60 @@
+const tasks = [
+  {
+    id: '56268018-3057-4a75-80f2-281df7221c9a',
+    title: 'Complete Redux tutoral',
+    description: 'Finish effects, state, reducers',
+    project: 'wepala',
+    created: '2020-04-10',
+    dueDate: '2020-05-10',
+  },
+  {
+    id: '56268018-3057-4a75-80f2-281df7221c9b',
+    title: 'Complete React tutoral',
+    description: 'Finish effects, state, reducers',
+    project: 'wepala',
+    created: '2020-04-10',
+    dueDate: '2020-05-10',
+  },
+  {
+    id: '56268018-3057-4a75-80f2-281df7221c9c',
+    title: 'Complete PHP tutoral',
+    description: 'Finish effects, state, reducers',
+    project: 'wepala',
+    created: '2020-04-10',
+    dueDate: '2020-05-10',
+  },
+  {
+    id: '56268018-3057-4a75-80f2-281df7221c9d',
+    title: 'Complete Go tutoral',
+    description: 'Finish effects, state, reducers',
+    project: 'wepala',
+    created: '2020-04-10',
+    dueDate: '2020-05-10',
+  },
+  {
+    id: '56268018-3057-4a75-80f2-281df7221c9e',
+    title: 'Complete Vue tutoral',
+    description: 'Finish effects, state, reducers',
+    project: 'wepala',
+    created: '2020-04-10',
+    dueDate: '2020-05-10',
+  },
+  {
+    id: '56268018-3057-4a75-80f2-281df7221c9f',
+    title: 'Complete Python tutoral',
+    description: 'Finish effects, state, reducers',
+    project: 'wepala',
+    created: '2020-04-10',
+    dueDate: '2020-05-10',
+  },
+  {
+    id: '56268018-3057-4a75-80f2-281df7221c9g',
+    title: 'Complete Ruby tutoral',
+    description: 'Finish effects, state, reducers',
+    project: 'wepala',
+    created: '2020-04-10',
+    dueDate: '2020-05-10',
+  },
+];
+
+export default tasks;
