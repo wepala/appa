077695732c0b039581a537b4ada125ae6c7d8f6c
akeem <akeem.philbert@wepala.com>
2020-05-09 18:07:40 -0400

    AGENDA-77 Create selector logic for getting and reducing the time for a given task
    * Added selector for getting time by date
    * Added selectors to get total time spent on tasks and task by date

--- src/logs/model/__tests__/selectors.spec.js
+++ src/logs/model/__tests__/selectors.spec.js
@@ -1,6 +1,13 @@
 import {mockLogs} from '../../__tests__/fixtures';
-import {getLogsByTaskId} from '../selectors';
+import {
+  getLogsByTaskId,
+  getTimeSpentByTaskId,
+  getTimeSpentByDay,
+} from '../selectors';
 import {mockTasks} from '../../../tasks/__tests__/fixtures';
+import {addTimeLog} from '../commands';
+import logs from '../reducer';
+import moment from 'moment';
 
 describe('Time Log Selectors', () => {
   it('should get task logs', () => {
@@ -14,10 +21,58 @@ describe('Time Log Selectors', () => {
     );
     expect(logs).toBeArrayOfSize(2);
     expect(logs[0]).toBe(
-      mockLogs.getById['f4cb9236-2df7-4abd-8c06-cb836865a1c3'],
+      mockLogs.getById.get('f4cb9236-2df7-4abd-8c06-cb836865a1c3'),
     );
     expect(logs[1]).toBe(
-      mockLogs.getById['8f38fb66-ddbd-4762-a125-17f81a1a5f5a'],
+      mockLogs.getById.get('8f38fb66-ddbd-4762-a125-17f81a1a5f5a'),
     );
+  });
+
+  it('should have a method for getting time spent on a task', () => {
+    const timeSpent = getTimeSpentByTaskId(
+      mockLogs,
+      '36212c03-040b-4139-867f-bd76485f4084',
+    );
+    expect(timeSpent).toBe(7200);
+  });
+
+  it('should have a method for getting time spent on a task on a given day', () => {
+    const timeSpent = getTimeSpentByDay(
+      mockLogs,
+      '36212c03-040b-4139-867f-bd76485f4084',
+      '2020-05-08',
+    );
+    expect(timeSpent).toBe(3600);
+  });
+
+  it('should only recompute if state is updated or paramter changed', () => {
+    const timeSpent = getTimeSpentByDay(
+      mockLogs,
+      '36212c03-040b-4139-867f-bd76485f4084',
+      '2020-05-08',
+    );
+    expect(timeSpent).toBe(3600);
+    getTimeSpentByDay(
+      mockLogs,
+      '36212c03-040b-4139-867f-bd76485f4084',
+      '2020-05-08',
+    );
+    expect(getTimeSpentByDay.recomputations()).toBe(1);
+    getTimeSpentByDay(
+      mockLogs,
+      '36212c03-040b-4139-867f-bd76485f4084',
+      '2020-05-08',
+    );
+    expect(getTimeSpentByDay.recomputations()).toBe(1);
+    const updatedState = logs(
+      mockLogs,
+      addTimeLog('36212c03-040b-4139-867f-bd76485f4084', moment().format()),
+    );
+    getTimeSpentByDay(
+      updatedState,
+      '36212c03-040b-4139-867f-bd76485f4084',
+      '2020-05-08',
+    );
+    expect(getTimeSpentByDay.recomputations()).toBe(2);
   });
 });
--- src/logs/model/selectors.js
+++ src/logs/model/selectors.js
@@ -1,11 +1,57 @@
 import {createSelector} from 'reselect';
+import moment from 'moment';
 
 const getByTaskSelector = (state, taskId) => state.getByTaskId.get(taskId);
 const getByIdSelector = state => state.getById;
-const getByTimeSelector = state => state.getByTime;
+const getByTimeSelector = state => [...state.getByTime.keys()];
 
-
+const getTimesByDate = (state, taskId, date) =>
+  taskId !== undefined && taskId !== null
+    ? [...state.getByTime.keys()]
+        .filter(time => time.startsWith(date))
+        .filter(
+          time =>
+            state.getById.get(state.getByTime.get(time))?.taskId === taskId,
+        )
+    : [...state.getByTime.keys()].filter(time => time.startsWith(date));
+/**
+ * get the amount of time spent by day
+ *
+ * @type {OutputSelector<unknown, boolean|AnimatedNode<number>|number, (res1: unknown[], res2: unknown[]) => (boolean|AnimatedNode<number>|number)>}
+ */
+export const getTimeSpentByDay = createSelector(
+  [getTimesByDate, getByTimeSelector],
+  (timesByDate, times) => {
+    return timesByDate
+      .map(time => moment(times[times.indexOf(time) + 1]).diff(time, 'seconds'))
+      .reduce((totalTime, time) => totalTime + time);
+  },
+);
+/**
+ * Get logs by task id
+ *
+ * @type {OutputSelector<unknown, *, (res1: unknown, res2: ({'de2c2c26-cadb-4f06-a819-b1ffcfba35b0': {agendas: [string], dueDate: string, description: string, id: string, title: string, complete: boolean, billable: boolean}, 'caac3188-4666-4b1f-9ad8-20c65a2871d2': {agendas: [string], dueDate: string, description: string, id: string, title: string, complete: boolean, billable: boolean}, '7a5fe6af-27f5-486b-a32d-4d3d0437d0c3': {agendas: [string], dueDate: string, description: string, id: string, title: string, complete: boolean, billable: boolean}, '22dc4620-66c8-4c02-ac48-3c030d48bfee': {agendas: [string], dueDate: string, description: string, project: string, id: string, title: string, complete: boolean, billable: boolean}, '36212c03-040b-4139-867f-bd76485f4084': {agendas: [string], dueDate: string, description: string, project: string, id: string, title: string, complete: boolean, billable: boolean}}|{})) => *>}
+ */
 export const getLogsByTaskId = createSelector(
   [getByTaskSelector, getByIdSelector],
-  (logIds, logs) => logIds.map(logId => logs[logId]),
+  (logIds, logs) => logIds.map(logId => logs.get(logId)),
+);
+/**
+ * Get the amount of time spent by task id
+ *
+ * @type {OutputSelector<unknown, *, (res1: unknown, res2: ({'de2c2c26-cadb-4f06-a819-b1ffcfba35b0': {agendas: [string], dueDate: string, description: string, id: string, title: string, complete: boolean, billable: boolean}, 'caac3188-4666-4b1f-9ad8-20c65a2871d2': {agendas: [string], dueDate: string, description: string, id: string, title: string, complete: boolean, billable: boolean}, '7a5fe6af-27f5-486b-a32d-4d3d0437d0c3': {agendas: [string], dueDate: string, description: string, id: string, title: string, complete: boolean, billable: boolean}, '22dc4620-66c8-4c02-ac48-3c030d48bfee': {agendas: [string], dueDate: string, description: string, project: string, id: string, title: string, complete: boolean, billable: boolean}, '36212c03-040b-4139-867f-bd76485f4084': {agendas: [string], dueDate: string, description: string, project: string, id: string, title: string, complete: boolean, billable: boolean}}|{}), res3: unknown[]) => *>}
+ */
+export const getTimeSpentByTaskId = createSelector(
+  [getByTaskSelector, getByIdSelector, getByTimeSelector],
+  (logIds, logs, times) => {
+    //do a map reduce such that we sum the time difference between the log item we're looking at and the adjacent log
+    return logIds
+      .map(logId =>
+        moment(times[times.indexOf(logs.get(logId).startTime) + 1]).diff(
+          logs.get(logId).startTime,
+          'seconds',
+        ),
+      )
+      .reduce((totalTime, time) => totalTime + time);
+  },
 );
