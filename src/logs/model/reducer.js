2ed0cb96340ecec303607ecf5d694632a24628fd
akeem <akeem.philbert@wepala.com>
2020-05-09 14:16:21 -0400

    AGENDA-22 switched to using maps

--- src/logs/model/reducer.js
+++ src/logs/model/reducer.js
@@ -2,46 +2,50 @@ import {v4 as uuidv4} from 'uuid';
 import {ADD_LOG} from './commandTypes';
 
 const logs = (
-  state = {getByTaskId: [], getById: {}, getByTime: []},
+  state = {getByTaskId: new Map(), getById: new Map(), getByTime: new Map()},
   action,
 ) => {
-  let getById = {};
-  let getByTaskId = [];
-  let getByTime = [];
+  let getById = new Map();
+  let getByTaskId = new Map();
+  let getByTime = new Map();
 
   switch (action.type) {
     case ADD_LOG:
+      //deep copy the getById
+      for (let [key, value] of state.getById) {
+        getById.set(key, value);
+      }
+
       //deep copy the getByTaskId
-      Object.keys(state.getById).map(id => {
-        getById = Object.assign({}, getById, {
-          [id]: state.getById[id],
-        });
-      });
+      for (let [key, value] of state.getByTaskId) {
+        getByTaskId.set(key, value);
+      }
+
+      //deep copy the getByTime
+      for (let [key, value] of state.getByTime) {
+        getByTime.set(key, value);
+      }
+
       //add the new time log
       const generatedId = uuidv4();
-      getById = Object.assign({}, getById, {
-        [generatedId]: Object.assign({}, action.payload, {
+      getById.set(
+        generatedId,
+        Object.assign({}, action.payload, {
           id: generatedId,
           meta: {
             id: generatedId,
           },
         }),
-      });
+      );
       //add to the task id index
-      state.getByTaskId.forEach((value, index) => {
-        getByTaskId[index] = value;
-      });
-      if (getByTaskId[action.payload.taskId] !== undefined) {
-        getByTaskId[action.payload.taskId].push(generatedId);
+      if (getByTaskId.has(action.payload.taskId)) {
+        getByTaskId.get(action.payload.taskId).push(generatedId);
       } else {
-        getByTaskId[action.payload.taskId] = [generatedId];
+        getByTaskId.set(action.payload.taskId, [generatedId]);
       }
 
-      //add to the timeindex
-      state.getByTime.forEach((value, index) => {
-        getByTime[index] = value;
-      });
-      getByTaskId[action.payload.startTime] = generatedId;
+      //add to the time index
+      getByTime.set(action.payload.startTime, generatedId);
 
       //return the updated state
       return Object.assign({}, state, {
