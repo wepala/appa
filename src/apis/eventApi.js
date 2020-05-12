665a488ec6ebe4f5bed992a7df887009135ef93a
chad <timothy.kyadondo@gmail.com>
2020-05-12 19:14:25 +0300

    AGENDA-100: Update get endpoint

--- src/apis/eventApi.js
+++ src/apis/eventApi.js
@@ -1,7 +1,7 @@
 import {eventsApi} from './calls';
 
 export async function fetchEvents() {
-  const response = await eventsApi.get('/events');
+  const response = await eventsApi.get('/events/get');
 
   if (response.status >= 400) {
     throw new Error(response.data.error);
