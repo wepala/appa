f85baa2dda2d4c56eaaf1820e779628ac72aca77
chad <timothy.kyadondo@gmail.com>
2020-05-12 19:01:08 +0300

    AGENDA-100: Update the application ID

--- src/store.js
+++ src/store.js
@@ -12,7 +12,7 @@ const initialState = {
   },
   eventCount: 0,
   token: null,
-  applicationId: '36212c03-040b-4139-867f-bd76485f4084', // TODO add actual applicationId
+  applicationId: '747f53e4-8552-428b-a50d-945bbfff2bdd', // TODO add actual applicationId
 };
 const middlewares = [thunk];
 
