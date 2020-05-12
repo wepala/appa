cb431a8406d5234ec1657604921639074e303c6a
Simon Sankar <simon.sankar@wepala.com>
2020-05-12 14:36:02 -0400

    AGENDA-27 Fixed test on Logs.spec

--- src/logs/controllers/Logs.js
+++ src/logs/controllers/Logs.js
@@ -1,24 +1,11 @@
 import {connect} from 'react-redux';
 import {Controller} from '../../controller';
 import {getLogsByFilter} from '../model/selectors';
-import {tasksSelector} from '../../tasks/model/selectors';
 
 export default class LogsController extends Controller {
   configureState(state, props): {} {
-    let tasks = tasksSelector(state);
-    let logs = getLogsByFilter(state, props);
-    logs = logs.map(log => {
-      let taskIndex = tasks.findIndex(task => {
-        return task.id === log.taskId;
-      });
-      return {
-        ...log,
-        ...tasks[taskIndex],
-      };
-    });
-    console.log(tasks);
     return {
-      items: logs,
+      items: getLogsByFilter(state, props),
     };
   }
 }
