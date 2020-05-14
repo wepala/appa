3360731a320e8645ce27a6266d58bf47c9809403
chad <timothy.kyadondo@gmail.com>
2020-05-14 20:29:10 +0300

    AGENDA-100: Dispatch events

--- src/views/Sync.js
+++ src/views/Sync.js
@@ -1,9 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
 import {fetchEvents} from '../apis/eventApi';
-import * as taskActionCreators from '../tasks/model/commands';
-import {REMOVE_TASK, UPDATE_TASK, ADD_TASK} from '../tasks/model/commandTypes';
 import {SyncSpinner} from '../views/components/Spinners';
 import {setEventCount} from '../actions';
 
@@ -14,23 +11,13 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProps = dispatch => {
-  return bindActionCreators(
-    {
-      ...taskActionCreators,
-      updateEventCount: setEventCount,
-    },
+  return {
+    updateEventCount: setEventCount,
     dispatch,
-  );
+  };
 };
 
-const Sync = ({
-  eventCount,
-  children,
-  addTask,
-  removeTask,
-  updateTask,
-  updateEventCount,
-}) => {
+const Sync = ({eventCount, children, updateEventCount, dispatch}) => {
   const [isSyncComplete, setSyncComplete] = useState(false);
 
   useEffect(() => {
@@ -40,18 +27,8 @@ const Sync = ({
           for (let event of data.events) {
             delete event.meta;
 
-            switch (event.type) {
-              case ADD_TASK:
-                addTask(event.payload);
-                break;
-              case UPDATE_TASK:
-                event.payload.meta = {id: event.payload.id};
-                updateTask(event.payload.id, event.payload);
-                break;
-              case REMOVE_TASK:
-                removeTask(event.payload.id);
-                break;
-            }
+            event.meta = {id: event.payload.id};
+            dispatch(event);
           }
 
           updateEventCount(data.currentCount);
