b7bc63a4cb6821d574220ad3afe9256a3f093027
chad <timothy.kyadondo@gmail.com>
2020-05-21 15:18:55 +0300

    AGENDA-141: Add update and remove reducers

--- src/logs/model/__tests__/reducer.spec.js
+++ src/logs/model/__tests__/reducer.spec.js
@@ -1,6 +1,7 @@
 import logs from '../reducer';
 import moment from 'moment';
-import {addTimeLog} from '../commands';
+import {addTimeLog, updateTimeLog, removeTimeLog} from '../commands';
+import {mockLogs} from '../../__tests__/fixtures';
 
 describe('logs reducer', function() {
   const expectedInitialState = {
@@ -27,5 +28,60 @@ describe('logs reducer', function() {
     expect(state.getByTaskId.size).toEqual(1);
     expect(state.getByTaskId.get(taskId)[0]).toBe(key);
     expect(state === expectedInitialState).toBe(false);
+  });
+
+  it('should handle updating a log', () => {
+    const today = moment();
+    const mockedUpdateLog = {
+      id: 'f4cb9236-2df7-4abd-8c06-cb836865a1c3',
+      taskId: '45bd1547-c867-40fd-b602-64dd4ed7d69e',
+      startTime: today,
+    };
+
+    const state = mockLogs;
+    const timeLogState = logs(
+      state,
+      updateTimeLog(
+        mockedUpdateLog.id,
+        mockedUpdateLog.taskId,
+        mockedUpdateLog.startTime,
+      ),
+    );
+
+    let log = state.getById.get(mockedUpdateLog.id);
+
+    expect(timeLogState.getByTime.get(log.startTime)).not.toBeDefined();
+    expect(timeLogState.getByTaskId.get(log.taskId)).not.toEqual(
+      expect.arrayContaining([log.id]),
+    );
+
+    expect(timeLogState.getById.get(mockedUpdateLog.id).taskId).toBe(
+      mockedUpdateLog.taskId,
+    );
+
+    expect(timeLogState.getById.get(mockedUpdateLog.id).startTime).toBe(
+      mockedUpdateLog.startTime,
+    );
+
+    expect(timeLogState.getByTaskId.get(mockedUpdateLog.taskId)).toEqual(
+      expect.arrayContaining([mockedUpdateLog.id]),
+    );
+
+    expect(timeLogState.getByTime.get(mockedUpdateLog.startTime)).toEqual(
+      mockedUpdateLog.id,
+    );
+  });
+
+  it('should handle removing a log', () => {
+    const logToRemove = 'f4cb9236-2df7-4abd-8c06-cb836865a1c3';
+    const state = mockLogs;
+    const timeLogState = logs(state, removeTimeLog(logToRemove));
+    let log = state.getById.get(logToRemove);
+
+    expect(timeLogState.getByTime.get(log.startTime)).not.toBeDefined();
+    expect(timeLogState.getByTaskId.get(log.taskId)).not.toEqual(
+      expect.arrayContaining([log.id]),
+    );
+    expect(timeLogState.getById.get(log.id)).not.toBeDefined();
   });
 });
