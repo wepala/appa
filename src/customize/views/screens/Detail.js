1042a4f118c3bff295c18f8a73cfeab14270b8e1
Simon Sankar <simon.sankar@wepala.com>
2020-05-28 13:57:04 -0400

    AGENDA-147 Added form data customize request

--- src/customize/views/screens/Detail.js
+++ src/customize/views/screens/Detail.js
@@ -10,15 +10,21 @@ import {
   SelectItem,
   useStyleSheet,
   Text,
+  Modal,
+  Card,
+  IndexPath,
 } from '@ui-kitten/components';
 import {ArrowDownIcon} from '../../../views/components/Icons';
 import DetailTopBar from '../components/DetailTopBar';
+import {useForm} from '../../../weosHelpers';
 
 export default ({navigation, route, status, makeRequest}) => {
-  const [form, setForm] = useState({
+  const reasons = ['Bug', 'Feature'];
+
+  const [form, setForm] = useForm({
     name: '',
     email: '',
-    reason: '',
+    reason: new IndexPath(0),
     details: '',
   });
   const styles = useStyleSheet(themedStyles);
@@ -36,33 +42,81 @@ export default ({navigation, route, status, makeRequest}) => {
             Appa Modification Request
           </Text>
           <Layout style={styles.row}>
-            <Text category="s2">Status:{status}</Text>
             <Layout style={styles.column}>
-              <Input style={styles.paddingBottom} placeholder="Your name" />
-              <Input style={styles.paddingBottom} placeholder="Email Address" />
+              <Input
+                style={styles.paddingBottom}
+                placeholder="Your name"
+                onChangeText={(val) => {
+                  setForm(val.trimLeft(), 'name');
+                }}
+              />
+              <Input
+                style={styles.paddingBottom}
+                placeholder="Email Address"
+                keyboardType="email-address"
+                onChangeText={(val) => {
+                  setForm(val.trimLeft(), 'email');
+                }}
+              />
               <Select
                 style={[styles.select, styles.paddingBottom]}
                 placeholder="Select reason"
-                accessoryRight={ArrowDownIcon}>
-                <SelectItem title="Bug" />
-                <SelectItem title="Feature" />
+                accessoryRight={ArrowDownIcon}
+                value={reasons[form.reason.row]}
+                selectedIndex={form.reason}
+                onSelect={(index) => {
+                  console.log(index);
+                  setForm(index, 'reason');
+                }}>
+                {reasons.map((reason, index) => (
+                  <SelectItem key={index + ''} title={reason} />
+                ))}
               </Select>
               <Input
                 style={styles.paddingBottom}
                 placeholder="Type your message"
                 multiline={true}
                 numberOfLines={5}
+                onChangeText={(val) => {
+                  setForm(val.trimLeft(), 'details');
+                }}
               />
               <Button
+                disabled={
+                  form.name === '' ||
+                  form.email === '' ||
+                  form.details === '' ||
+                  status === 'success'
+                }
                 size="large"
                 style={styles.buttonSend}
                 onPress={() => {
-                  makeRequest(form);
-                  console.log(status);
+                  let data = {
+                    ...form,
+                    reason: reasons[form.reason.row],
+                  };
+                  console.log('Sending', data);
+                  makeRequest(data);
                 }}>
-                SEND REQUEST
+                {status === 'pending' ? 'SENDING...' : 'SEND REQUEST'}
               </Button>
             </Layout>
+            <Modal
+              visible={status !== 'init'}
+              style={styles.container}
+              backdropStyle={styles.backdrop}>
+              {status === 'error' ? (
+                <Card disabled={true}>
+                  <Text category="h3">Failed to Send</Text>
+                  <Button onPress={() => navigation.goBack()}>DISMISS</Button>
+                </Card>
+              ) : status === 'success' ? (
+                <Card disabled={true}>
+                  <Text category="h3">Request Sent!</Text>
+                  <Button onPress={() => navigation.goBack()}>DISMISS</Button>
+                </Card>
+              ) : null}
+            </Modal>
           </Layout>
         </LinearGradient>
       </KeyboardAvoidingView>
@@ -77,6 +131,9 @@ const themedStyles = StyleService.create({
   container: {
     display: 'flex',
     flex: 1,
+  },
+  backdrop: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
   row: {
     display: 'flex',
