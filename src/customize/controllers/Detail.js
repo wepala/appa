b7af37578e458b468272b7a0fe6331176e20e386
Simon Sankar <simon.sankar@wepala.com>
2020-06-04 12:05:10 -0400

    AGENDA-147 Removed logs

--- src/customize/controllers/Detail.js
+++ src/customize/controllers/Detail.js
@@ -1,32 +1,13 @@
-import React, {createContext} from 'react';
+import React from 'react';
 import axios from 'axios';
 import {SUPPORT_URL, SENDER, DESTINATION, BEARER} from 'react-native-dotenv';
 
 import DetailScreen from '../views/screens/Detail';
-import {RequestContext} from '../../weosHelpers';
 import {useState} from 'react';
-import {useEffect} from 'react';
 
-const value = {
-  status: null,
-  makeRequest(form) {
-    this.status = 'pending';
-  },
-};
 const Detail = (props) => {
   const [status, setStatus] = useState('init');
   const makeRequest = (form) => {
-    console.log(
-      SUPPORT_URL,
-      '\n',
-      BEARER,
-      '\n',
-      SENDER,
-      '\n',
-      DESTINATION,
-      '\n',
-      form,
-    );
     setStatus('pending');
     axios({
       method: 'post',
@@ -40,11 +21,9 @@ const Detail = (props) => {
       },
     })
       .then((res) => {
-        console.log('Success!!', res.data);
         setStatus('success');
       })
       .catch((error) => {
-        console.log('Error!!', error);
         setStatus('error');
       });
   };
