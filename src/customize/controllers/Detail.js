30cc769be7a2373ef80512c698fa29c0158b8c56
Simon Sankar <simon.sankar@wepala.com>
2020-06-09 14:30:54 -0400

    AGENDA-170 Added timespent | original est time to BacklogItem  - Added selector for timeSpent on incomplete tasks  - Updated Backlog controller

--- src/customize/controllers/Detail.js
+++ src/customize/controllers/Detail.js
@@ -1,11 +1,9 @@
 import React, {createContext} from 'react';
 import axios from 'axios';
-import {SUPPORT_URL, SENDER, DESTINATION, BEARER} from 'react-native-dotenv';
+import {SUPPORT_URL, SENDER, DESTINATION} from 'react-native-dotenv';
 
 import DetailScreen from '../views/screens/Detail';
-import {RequestContext} from '../../weosHelpers';
 import {useState} from 'react';
-import {useEffect} from 'react';
 
 const value = {
   status: null,
@@ -16,24 +14,11 @@ const value = {
 const Detail = (props) => {
   const [status, setStatus] = useState('init');
   const makeRequest = (form) => {
-    console.log(
-      SUPPORT_URL,
-      '\n',
-      BEARER,
-      '\n',
-      SENDER,
-      '\n',
-      DESTINATION,
-      '\n',
-      form,
-    );
+    console.log(SUPPORT_URL, '\n', SENDER, '\n', DESTINATION, '\n', form);
     setStatus('pending');
     axios({
       method: 'post',
       url: SUPPORT_URL,
-      headers: {
-        Authorization: BEARER,
-      },
       data: {
         meta: {
           sender: SENDER,
