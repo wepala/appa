eed7fbcf24b5cbe24c6e28af9409f8f85b35a255
Simon Sankar <simon.sankar@wepala.com>
2020-06-15 13:40:29 -0400

    AGENDA-183 Added placeholder message in backlog when there are no backlog items

--- src/tasks/views/components/Backlog.js
+++ src/tasks/views/components/Backlog.js
@@ -1,7 +1,26 @@
-import {List, StyleService, useStyleSheet} from '@ui-kitten/components';
-import React, {useEffect, useContext} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
+import {
+  Layout,
+  Text,
+  List,
+  StyleService,
+  useStyleSheet,
+} from '@ui-kitten/components';
 import BacklogItem from './BacklogItem';
 import {SectionContext} from '../../context/section-context';
+
+const messages = [
+  {
+    title: 'Your backlog is where all your unfinished tasks go.',
+    subTitle:
+      'You can quickly move them into your day’s agenda when tasks roll over into other days.',
+  },
+  {
+    title: 'Yay, your backlog is empty!',
+    subTitle:
+      'This means you have no unfinished tasks. You can quickly move backlog tasks to your day’s agenda.',
+  },
+];
 
 export default ({
   navigation,
@@ -11,6 +30,9 @@ export default ({
   setTaskCompletion,
   timeTotals,
 }) => {
+  const [placeholder, setPlaceholder] = useState(
+    messages[Math.floor(Math.random() * 2)],
+  );
   const styles = useStyleSheet(themedStyles);
   const onItemPress = (index) => {
     navigation.navigate('UpdateTask', {
@@ -39,19 +61,28 @@ export default ({
     />
   );
 
-  return (
+  return items.length ? (
     <List
       style={[styles.container, contentContainerStyle]}
       numColumns={1}
       data={items}
       renderItem={renderItem}
     />
+  ) : (
+    <Layout style={styles.placeholder}>
+      <Text style={styles.title} category="h3" appearance="hint">
+        {placeholder.title}
+      </Text>
+      <Text style={styles.subTitle} category="h6" appearance="hint">
+        {placeholder.subTitle}
+      </Text>
+    </Layout>
   );
 };
 
 const themedStyles = StyleService.create({
   container: {
-    paddingVertical: 16,
+    padding: 16,
     backgroundColor: '$background-basic-color-2',
     flex: 1,
   },
@@ -59,5 +90,21 @@ const themedStyles = StyleService.create({
     flex: 1,
     padding: 0,
     backgroundColor: 'transparent',
+  },
+  placeholder: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+    padding: 16,
+  },
+  title: {
+    textAlign: 'center',
+    marginBottom: 32,
+  },
+  subTitle: {
+    textAlign: 'center',
   },
 });
