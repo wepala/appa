3aa8be70503b2ac9220d127fd62aec02b91b1f74
KevinChristopher93 <kevin.christopher@wepala.com>
2020-06-16 11:37:44 -0400

    AGENDA-186 adjusted the description text and alignment of icon.

--- src/customize/views/screens/Customize.js
+++ src/customize/views/screens/Customize.js
@@ -1,20 +1,14 @@
 import React, {useState} from 'react';
-import {ScrollView, Linking} from 'react-native';
+import {ScrollView, Linking, Platform} from 'react-native';
 import {
   Button,
   Layout,
   StyleService,
-  Select,
-  SelectItem,
   useStyleSheet,
   Text,
   Card,
-  Icon,
-  IndexPath,
   Modal,
-  Divider,
 } from '@ui-kitten/components';
-import {MessageIcon} from '../../../views/components/Icons';
 import TopBar from '../components/TopBar';
 import {SafeAreaView} from 'react-native';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
@@ -25,63 +19,69 @@ export default ({navigation, route}) => {
   let [visible, toggleVisible] = useState(false);
 
   return (
-    <SafeAreaView style={styles.container}>
+      <SafeAreaView style={styles.container}>
       <TopBar navigation={navigation} title="Customize" />
       <ScrollView>
-        <Text category="h1" style={styles.title}>
-          Make Appa yours
-        </Text>
-        <Layout style={styles.row}>
-          <Layout style={styles.column}>
-            <Text category="h5" style={styles.subTitle}>
-              DEVELOPERS
-            </Text>
+      <Text category="h1" style={styles.title}>
+      Make Appa yours
+  </Text>
+  <Layout style={styles.row}>
+      <Layout style={styles.column}>
+      <Text category="h5" style={styles.subTitle}>
+      DEVELOPERS
+      </Text>
 
-            <Text category="s1" style={styles.description}>
-              If you can code then jump right in and make the changes you want!
-              Our code is free and open source!
-            </Text>
+      <Text category="s1" style={styles.description}>
+      If you can code, then jump right in and make the changes you want!
+  </Text>
+  <Text category="s1" style={styles.descriptionLastLine}>
+      Check out Appa on GitHub.
+  </Text>
 
-            <Button
-              size="large"
-              style={styles.buttonHelp}
-              onPress={() => {
-                Linking.openURL('https://github.com/wepala/weagenda').catch(
-                  (err) => {
-                    toggleVisible(true);
-                    console.warn(err);
-                  },
-                );
-              }}>
-              GET STARTED{' '}
-              <FontAwesomeIcon style={styles.gitButton} icon={faGithub} />
-            </Button>
-          </Layout>
-          <Layout style={styles.column}>
-            <Text category="h5" style={styles.subTitle}>
-              NON-DEVELOPERS
-            </Text>
-            <Text category="s1" style={styles.description}>
-              Need some help modifying Appa to work the way you want? No
-              problem, we;ll take of it for you.
-            </Text>
-            <Button
-              size="large"
-              style={styles.buttonHelp}
-              onPress={() => navigation.navigate('Help')}>
-              GET HELP
-            </Button>
-          </Layout>
-        </Layout>
-        <Modal visible={visible} style={styles.container}>
-          <Card disabled={true}>
-            <Text category="h3">Error</Text>
-            <Button onPress={() => toggleVisible(false)}>DISMISS</Button>
-          </Card>
-        </Modal>
-      </ScrollView>
-    </SafeAreaView>
-  );
+  <Button
+  size="large"
+  style={styles.buttonHelp}
+  onPress={() => {
+    Linking.openURL('https://github.com/wepala/weagenda').catch(
+        (err) => {
+          toggleVisible(true);
+          console.warn(err);
+        },
+    );
+  }}
+  accessoryRight={() => (
+  <FontAwesomeIcon style={styles.icon} icon={faGithub} />
+)}>
+  GET STARTED
+  </Button>
+  </Layout>
+  <Layout style={styles.column}>
+      <Text category="h5" style={styles.subTitle}>
+      NON-DEVELOPERS
+      </Text>
+      <Text category="s1" style={styles.description}>
+      Need some help modifying Appa to work the way you want?
+                                    </Text>
+  <Text category="s1" style={styles.descriptionLastLine}> No
+      problem, we'll take care of it for you.
+  </Text>
+                                    <Button
+    size="large"
+  style={styles.buttonHelp}
+  onPress={() => navigation.navigate('Help')}>
+  GET HELP
+  </Button>
+  </Layout>
+  </Layout>
+  <Modal visible={visible} style={styles.container}>
+      <Card disabled={true}>
+      <Text category="h3">Error</Text>
+      <Button onPress={() => toggleVisible(false)}>DISMISS</Button>
+  </Card>
+  </Modal>
+  </ScrollView>
+  </SafeAreaView>
+);
 };
 
 const themedStyles = StyleService.create({
@@ -110,11 +110,19 @@ const themedStyles = StyleService.create({
   description: {
     textAlign: 'center',
     paddingHorizontal: 32,
+  },
+  descriptionLastLine: {
+    textAlign: 'center',
+    paddingHorizontal: 32,
     paddingBottom: 32,
   },
   buttonHelp: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
     width: '100%',
     marginBottom: 32,
+    lineHeight: 90,
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
@@ -123,16 +131,15 @@ const themedStyles = StyleService.create({
     shadowOpacity: 0.1,
     shadowRadius: 7,
     elevation: 5,
+    paddingTop: Platform.OS === 'ios' ? 20 : 12,
+  },
+  icon: {
+    color: '$background-basic-color-1',
+    marginTop: Platform.OS === 'ios' ? -8 : 2,
   },
   modal: {
     marginTop: 8,
-    backgroundColor: 'white',
-    borderColor: 'white',
-  },
-  gitButton: {
-    color: 'white',
-    display: 'flex',
-    justifyContent: 'center',
-    verticalAlign: 'middle',
+    backgroundColor: '$background-basic-color-1',
+    borderColor: '$background-basic-color-1',
   },
 });
